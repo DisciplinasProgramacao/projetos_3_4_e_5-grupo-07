070a86e572ae1caee2cf323c7680673646d98679
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-05-30 19:15:55 -0300

    corrigida formatação do código

--- codigo/app/src/main/java/projeto/Avaliacao.java
+++ codigo/app/src/main/java/projeto/Avaliacao.java
@@ -15,24 +15,27 @@ public class Avaliacao {
 		this.nota = nota;
 	}
 
-
 	public float getNota() {
 		return nota;
 	}
+
 	public void setNota(float nota) {
 		this.nota = nota;
 	}
+
 	public String getComentario() {
 		return comentario;
 	}
+
 	public void setComentario(String comentario) {
 		this.comentario = comentario;
 	}
+
 	public String getData() {
 		return data;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
-	
 }
