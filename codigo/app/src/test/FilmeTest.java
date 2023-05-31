5b00cb073a01a8a5480046833805c04343b8379a
Saulo Pereira Vasconcelos <108476471+Saulin13@users.noreply.github.com>
2023-05-30 23:43:28 -0300

    Update FilmeTest.java

--- codigo/app/src/test/FilmeTest.java
+++ codigo/app/src/test/FilmeTest.java
@@ -1,1 +1,28 @@
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.security.InvalidParameterException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmeTest {
+    private Filme filme;
+
+    @BeforeEach
+    void setUp() throws InvalidParameterException {
+        filme = new Filme(1, "Filme Teste", "Romance","Portugues", 2010);
+    }
+
+    @Test
+    void getDuracaoTest() {
+        String nome = filme.getNome();
+        assertEquals("Filme Teste", nome);
+    }
+
+    
+    @Test
+    void toStringTest() {
+        String resultadoEsperado = "1;Filme Teste;Romance;Portugues;25/10/2000";
+        String resultadoObtido = filme.toString();
+        assertEquals(resultadoEsperado, resultadoObtido);
+    }
+}
