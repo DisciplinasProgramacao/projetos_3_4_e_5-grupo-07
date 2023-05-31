d62363529f560201981639d0ce89aa95863ad8af
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-05-30 22:30:22 -0300

    adicionada função para salvar dados em arquivos

--- codigo/app/src/main/java/projeto/App.java
+++ codigo/app/src/main/java/projeto/App.java
@@ -1,15 +1,15 @@
 package projeto;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 
 public class App {
 	static Scanner scanner = new Scanner(System.in);
+    static PlataformaStreaming plataforma = new PlataformaStreaming(); 
     
     public static void main(String[] args) throws IOException {
-        PlataformaStreaming plataforma = new PlataformaStreaming(); 
-        
         while (true) {
             System.out.println("Menu:");
             System.out.println("1. Fazer Login");
@@ -54,4 +54,22 @@ public class App {
 
     private static void cadastrarMidia(PlataformaStreaming plataforma) {
     }
+
+	public static void salvaArquivos() {
+		try (FileWriter filmes = new FileWriter("clientes.csv", false)) {
+			filmes.write(plataforma.salvarClientes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try (FileWriter filmes = new FileWriter("series.csv", false)) {
+			filmes.write(plataforma.salvarSeries());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try (FileWriter filmes = new FileWriter("filmes.csv", false)) {
+			filmes.write(plataforma.salvarFilmes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
