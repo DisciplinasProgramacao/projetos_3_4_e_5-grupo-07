996b2a4df88885830616e295a5a2a26f030c9e5c
Saulo Pereira Vasconcelos <108476471+Saulin13@users.noreply.github.com>
2023-06-23 16:37:03 -0300

    Update ClienteTest.java

--- codigo/app/src/test/ClienteTest.java
+++ codigo/app/src/test/ClienteTest.java
@@ -3,20 +3,61 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ClienteTest {
+	Private c1 Cliente;
+	Serie s1;
+	Serie s2;
+	Serie s3;
 
-	Cliente c1;
-				
+
+	@BeforeEach
+	void setUp() throws InvalidParameterException {
+		c1 = new Cliente("Joao","abcd");
+		s1 = new Serie ("Serie1", "Ação" ,"Pt-BR", "25/12/2000", 12  );
+		s2 = new Serie ("Serie2", "Comedia" ,"Ingles", "30/05/2005", 18  );
+		s3 = new Serie ("Serie3", "Drama" ,"Frances", "27/06/2010", 10  );
+	}
+
 	@Test
-	void testAddLista() {
-		c1 = new Cliente();
+	void AddNaListaTest() {
+		c1.adicionarNaLista(s1);
+		Assertions.assertTrue(cliente.getListaParaVer().contains(s1));
+		c1.adicionarNaLista(s2);
+		Assertions.assertTrue(cliente.getListaParaVer().contains(s));
+	}
 
-        c1.adicionarNaLista(Flash);
+	@Test
+	public void adicionarNoHistorico() {
 
-        assertEquals("Flash",  );
+		c1.adicionarNaLista(s1);
+		Assertions.assertTrue(cliente.getListaParaVer().contains(s1));
+	}
 
-		//Testes incompletos;
-		
-		
+
+	@Test
+	public void retirarDaListaTest() {
+
+		c1.adicionarNaLista(s1);
+		c1.adicionarNaLista(s2);
+
+		c1.retirarDaLista("Série 1");
+		Assertions.assertFalse(c1.getListaParaVer().contains(s1));
+		Assertions.assertTrue(c1.getListaParaVer().contains(s2));
+	}
+
+	@Test
+	public void testFiltrarPorIdioma() {
+		c1.adicionarNaLista(s1);
+		List<Midia> resultado = cliente.filtrarPorIdioma("Pt-BR");
+		assertEquals(1, resultado.size());
+	}
+
+	@Test
+	public void testFiltrarPorQtdEpisodios() {
+		c1.adicionarNaLista(s1);
+		c1.adicionarNaLista(s2);
+		c1.adicionarNaLista(s3);
+		List<Midia> resultado = c1.filtrarPorQtdEpisodios(10);
+		assertEquals(1, resultado.size());
 	}
 
 }
