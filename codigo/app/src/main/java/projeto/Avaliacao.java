12c26b480088134bf1efa695ef37799a6703c4ea
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-06-23 22:34:14 -0300

    removidos métodos

--- codigo/app/src/main/java/projeto/Avaliacao.java
+++ codigo/app/src/main/java/projeto/Avaliacao.java
@@ -19,16 +19,8 @@ public class Avaliacao {
 		return nota;
 	}
 
-	public void setNota(double nota) {
-		this.nota = nota;
-	}
-
 	public String getComentario() {
 		return comentario;
-	}
-
-	public void setComentario(String comentario) {
-		this.comentario = comentario;
 	}
 
 	public String getData() {
