159764e24e421c185061244e675f30317520d562
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-06-24 10:55:04 -0300

    adicionada classe lançamentos

---
+++ codigo/app/src/main/java/projeto/Lancamento.java
@@ -0,0 +1,16 @@
+package projeto;
+
+/**
+ * Lancamento
+ */
+public class Lancamento {
+	private Midia midia;
+
+	public Lancamento(Midia midia) {
+		this.midia = midia;
+	}
+
+	public Midia getMidia() {
+		return midia;
+	}
+}
