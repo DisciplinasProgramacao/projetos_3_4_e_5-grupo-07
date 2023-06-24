e8e8eb6fcbab69b3102646f84e4448e27fb7d2cc
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-06-24 12:32:05 -0300

    adicionado trailer

--- codigo/app/src/main/java/projeto/Trailer.java
+++ codigo/app/src/main/java/projeto/Trailer.java
@@ -4,6 +4,21 @@ package projeto;
  * Trailer
  */
 public class Trailer {
+	private Midia midia;
 
-	
+	public Trailer(Midia midia) {
+		this.midia = midia;
+	}
+
+	public Trailer(Lancamento midia) {
+		this(midia.getMidia());
+	}
+
+	public String getNomeMidia() {
+		return midia.getNome();
+	}
+	 
+	public Genero getGeneroMidia() {
+		return midia.getGenero();
+	}
 }
