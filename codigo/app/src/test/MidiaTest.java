9b6b34f44368b3a705b9995f1172990aa0dc0530
Saulo Pereira Vasconcelos <108476471+Saulin13@users.noreply.github.com>
2023-06-24 10:58:44 -0300

    Update MidiaTest.java

--- codigo/app/src/test/MidiaTest.java
+++ codigo/app/src/test/MidiaTest.java
@@ -4,9 +4,34 @@ import org.junit.jupiter.api.Test;
 
 class MidiaTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+
+	public void testCriandoFilme() {
+		String nome = "Filme Teste";
+		String genero = "Ação";
+		String idioma = "Português";
+		String dataLancamento = "20/05/2001";
+		Midia midia = new Filme(nome, idioma, genero, dataLancamento);
+		Assert.assertEquals("Filme Teste", midia.getNome());
+		Assert.assertEquals(idioma, midia.getIdioma());
+		Assert.assertEquals(genero, midia.getGenero());
 	}
 
+	@Test
+	public void deveAdicionarUmaAvaliacao() throws IllegalArgumentException, SenhaFracaException {
+
+		String nome = "Filme Teste";
+		String genero = "Ação";
+		String idioma = "Português";
+		String dataLancamento = "20/05/2001";
+		Cliente c1 = new Cliente("Joao","abcd");
+
+		Midia m = new Filme(nome, idioma, genero, dataLancamento);
+
+		Avaliacao a = new Avaliacao(c1.getLogin(), 4);
+
+		m.addAvaliacao(a);
+
+		Assert.assertEquals(1, m.getTodasAvaliacoes().size());
+
+	}
 }
